b21e60121b29a81a96f1896dd08f16590c34e268
spirran <31646656+spirran@users.noreply.github.com>
2022-12-14 14:57:07 +0100

    Fredrik

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,7 @@ public class    Main {
 
     public static void main(String[] args) {
         System.out.printf("hej");
+        System.out.println("tja");
         boolean quit = false;
         printActions();
         while(!quit) {
